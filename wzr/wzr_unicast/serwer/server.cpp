1775d3f10aa302ac5371fb5a22f38cd0c4069981
WitoldDziurdz <witolddzyurdz@gmail.com>
2018-03-12 21:48:23 +0300

    server

--- wzr/wzr_unicast/serwer/server.cpp
+++ wzr/wzr_unicast/serwer/server.cpp
@@ -21,44 +21,62 @@ struct Ramka                                    // g³ówna struktura s³u¿¹ca do p
 	float wartosc_oferty;
 };
 
-unicast_net *uni_receive;
+struct User {
+	unsigned long addr; 
+	int id; 
+};
+
+unicast_net *uni_reciv;
 unicast_net *uni_send;
 
-std::vector<unsigned long> klienci;
+std::vector<User*> klienci;
 Ramka ramka;
 StanObiektu stan;
 
 int main()
 {
-	uni_receive = new unicast_net(10001);
-	uni_send = new unicast_net(10002);
+	uni_reciv = new unicast_net(10002);
+	uni_send = new unicast_net(10001);
 
 	while (1) {
 		int rozmiar;
 		unsigned long adres;
-		rozmiar = uni_receive->reciv((char*)&ramka, &adres, sizeof(Ramka));
+		rozmiar = uni_reciv->reciv((char*)&ramka, &adres, sizeof(Ramka));
 		switch (ramka.typ) {
 		case typy_ramek::STAN_OBIEKTU:
-			printf("stan");
-
+			printf("ramka STAN_OBIEKTU");
+			break; 
+		case typy_ramek::NEW_PLAYER: 
+			printf("ramka NEW_PLAYER");
+			break; 
+		case typy_ramek::CLOSE_WINDOW:
+			printf("ramka CLOSE_WINDOW");
+			break;
 		}
+
+		
 		
 		bool found = false;
 		for (int i = 0; i < klienci.size(); i++) {
-			if (klienci[i] == adres) {
+			if (klienci[i]->addr == adres && klienci[i]->id == ramka.stan.iID) {
 				found = true;
 				break;
 			}
 		}
 
+		
+
 		if (!found) {
-			klienci.push_back(adres);
+			User* user = new User();
+			user->addr = adres;
+			user->id = ramka.stan.iID;
+			klienci.push_back(user);
 			printf("Connected %lu", adres);
 		}
 
 		for (int i = 0; i < klienci.size(); i++) {
 			printf("To %lu (%d)\n", klienci[i], klienci.size());
-			uni_send->send((char*)&ramka, klienci[i], sizeof(Ramka));
+			uni_send->send((char*)&ramka, klienci[i]->addr, sizeof(Ramka));
 		}
 	}
 
